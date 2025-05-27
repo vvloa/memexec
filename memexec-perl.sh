56f3622f39fdf5b5e782871cf1c5453658a54b0c
Root THC <root@proton.thc.org>
2025-05-27 08:41:53 +0100

    explain the O_CLOEXEC perl trick

--- memexec-perl.sh
+++ memexec-perl.sh
@@ -5,6 +5,7 @@
 #  385 - armv6l
 # 4314 - mipsel n64
 # 4354 - mipsel o32
+# $^F=255; will disable perl's default O_CLOEXEC.
 memexec(){ perl '-e$^F=255;for(319,279,385,4314,4354){($f=syscall$_,$",0)>0&&last};open($o,">&=".$f);print$o(<STDIN>);exec{"/proc/$$/fd/$f"}X,@ARGV' -- "$@";}
 # memexec(){ perl '-efor(319,279){($f=syscall$_,$",1)>0&&last};open($o,">&=".$f);print$o(<STDIN>);exec{"/proc/$$/fd/$f"}X,@ARGV' -- "$@";}
 # Example: cat /usr/bin/id | memexec -u
